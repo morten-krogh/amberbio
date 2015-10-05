5bb14984284a61341e258a553a82694dcc9e6f90
Morten Krogh <morten.krogh@amberbio.com>
2015-10-05 19:30:00 +0200

    knn

--- Source/KNNTrainingTestSelection.swift
+++ Source/KNNTrainingTestSelection.swift
@@ -9,7 +9,7 @@ class KNNTrainingTestSelectionState: PageState {
                 super.init()
                 name = "knn_training_test_selection"
                 title = astring_body(string: "k nearest neighbor classification")
-                info = "Select the samples for the training set.\n\nThe numbers in parenthesis represent the total number of samples and the number of samples currently in the training set respectively for that level.\n\nSelecting a level from a factor leads to inclusion of all samples with that level.\n\nDeselecting a level removes all samples with that level from the training set."
+                info = "Select the samples for the training set.\n\nThe numbers in parenthesis represent the total number of samples and the number of samples currently in the training set respectively for that level.\n\nSelecting a level from a factor leads to inclusion of all samples with that level.\n\nDeselecting a level removes all samples with that level from the training set.\n\nThe test set consists of all the samples that are not in the training set.\n\nTo continue, both the training and test set must contain at least one sample."
         }
 }
 
