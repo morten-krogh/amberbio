a1318bd4c16259be2b6d9b5cafd62fcda1e0b073
Morten Krogh <morten.krogh@amberbio.com>
2015-10-13 17:46:06 +0200

    supervised classification

--- Source/SupervisedClassificationParameterSelection.swift
+++ Source/SupervisedClassificationParameterSelection.swift
@@ -31,11 +31,6 @@ class SupervisedClassificationParameterSelection: Component, UITableViewDataSour
                 view = table_view
         }
 
-//        let classify_button = UIButton(type: .System)
-//        let info_label = UILabel()
-//        let k_label = UILabel()
-//        let text_field = UITextField()
-
         override func viewDidLoad() {
                 super.viewDidLoad()
 
@@ -48,74 +43,13 @@ class SupervisedClassificationParameterSelection: Component, UITableViewDataSour
                 let tap_recognizer = UITapGestureRecognizer(target: self, action: "tap_action")
                 tap_recognizer.cancelsTouchesInView = false
                 view.addGestureRecognizer(tap_recognizer)
-
-
-//                classify_button.setAttributedTitle(astring_font_size_color(string: "Classify", font: nil, font_size: 22, color: nil), forState: .Normal)
-//                classify_button.addTarget(self, action: "classify_action", forControlEvents: UIControlEvents.TouchUpInside)
-//                view.addSubview(classify_button)
-//
-//                info_label.text = "Select the number of nearest neighbors"
-//                info_label.font = font_body
-//                info_label.textAlignment = .Center
-//                view.addSubview(info_label)
-//
-//                k_label.text = "k = "
-//                k_label.font = font_body
-//                view.addSubview(k_label)
-//
-//                text_field.keyboardType = UIKeyboardType.NumbersAndPunctuation
-//                text_field.font = font_body
-//                text_field.textAlignment = .Center
-//                text_field.autocorrectionType = UITextAutocorrectionType.No
-//                text_field.borderStyle = UITextBorderStyle.Bezel
-//                text_field.layer.masksToBounds = true
-//                text_field.delegate = self
-//
-//                view.addSubview(text_field)
-//
-//                let tap_recognizer = UITapGestureRecognizer(target: self, action: "tap_action")
-//                view.addGestureRecognizer(tap_recognizer)
         }
-
-//        override func viewWillLayoutSubviews() {
-//                super.viewWillLayoutSubviews()
-//
-//                let width = view.frame.width
-//                let height = view.frame.height
-//
-//                let vertical_margin = height > 300 ? 25 : 20 as CGFloat
-//                var origin_y = height > 300 ? 40 : 30 as CGFloat
-//
-//                classify_button.sizeToFit()
-//                classify_button.center = CGPoint(x: width / 2, y: origin_y)
-//                origin_y = CGRectGetMaxY(classify_button.frame) + vertical_margin
-//
-//                info_label.sizeToFit()
-//                info_label.center = CGPoint(x: width / 2, y: origin_y + info_label.frame.height / 2)
-//
-//                origin_y = CGRectGetMaxY(info_label.frame) + vertical_margin + 10
-//
-//                k_label.sizeToFit()
-//
-//                let text_field_width = 80 as CGFloat
-//                let text_field_height = 40 as CGFloat
-//                let middle_margin = 10 as CGFloat
-//
-//                var origin_x = (width - text_field_width) / 2 - k_label.frame.width - middle_margin
-//                k_label.frame.origin = CGPoint(x: origin_x, y: origin_y + (text_field_height - info_label.frame.height) / 2)
-//
-//                origin_x += k_label.frame.width + middle_margin
-//
-//                text_field.frame = CGRect(x: origin_x, y: origin_y, width: text_field_width, height: text_field_height)
-//        }
 
         override func render() {
                 supervised_classification = (state.page_state as! SupervisedClassificationParameterSelectionState).supervised_classification
 
                 table_view.dataSource = self
                 table_view.delegate = self
-
-//                text_field.text = "\(knn_k_selection_state.knn.k)"
         }
 
         func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -237,10 +171,6 @@ class SupervisedClassificationParameterSelection: Component, UITableViewDataSour
                         break
                 }
         }
-
-
-
-
 
         func header_tap_action(sender: UITapGestureRecognizer) {
                 editing_text_field?.resignFirstResponder()
