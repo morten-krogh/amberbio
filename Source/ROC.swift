72945031045193439dc0a7df57fa22856a0529a9
Morten Krogh <morten.krogh@amberbio.com>
2015-10-15 10:47:33 +0200

    roc

--- Source/ROC.swift
+++ Source/ROC.swift
@@ -24,10 +24,11 @@ class ROC: TiledScrollViewDelegate {
         func draw(context context: CGContext, rect: CGRect) {
                 draw_box(context: context)
                 draw_ticks(context: context)
-
-
+                draw_axis_1_labels(context: context)
+                draw_axis_2_labels(context: context)
+                draw_axis_1_title(context: context)
+                draw_axis_2_title(context: context)
         }
-
 
         func draw_box(context context: CGContext) {
                 let box_upper_left = CGPoint(x: box_lower_left.x, y: box_upper_right.y)
@@ -44,11 +45,6 @@ class ROC: TiledScrollViewDelegate {
                         let start_point = CGPoint(x: point.x, y: point.y + tick_length / 2)
                         let end_point = CGPoint(x: point.x, y: point.y - tick_length / 2)
                         drawing_draw_line(context: context, start_point: start_point, end_point: end_point)
-                        let astring = astring_body(string: String(value_1))
-                        let point_text = CGPoint(x: point.x, y: point.y + tick_length)
-
-                        drawing_draw_attributed_text(context: context, attributed_text: astring, origin: point_text, horizontal: true)
-
                 }
 
                 for value_2 in [0.2, 0.4, 0.6, 0.8] {
@@ -60,6 +56,43 @@ class ROC: TiledScrollViewDelegate {
 
 
         }
+
+        func draw_axis_1_labels(context context: CGContext) {
+                for value_1 in [0, 0.2, 0.4, 0.6, 0.8, 1] {
+                        let point = value_to_point(value_1: value_1, value_2: 0)
+                        let str = value_1 == 0 ? "0" : value_1 == 1 ? "1" : String(value_1)
+                        let astring = astring_body(string: str)
+                        let point_text = CGPoint(x: point.x - astring.size().width / 2, y: point.y + tick_length)
+                        drawing_draw_attributed_text(context: context, attributed_text: astring, origin: point_text, horizontal: true)
+                }
+        }
+
+        func draw_axis_2_labels(context context: CGContext) {
+                for value_2 in [0, 0.2, 0.4, 0.6, 0.8, 1] {
+                        let point = value_to_point(value_1: 0, value_2: value_2)
+                        let str = value_2 == 0 ? "0" : value_2 == 1 ? "1" : String(value_2)
+                        let astring = astring_body(string: str)
+                        let point_text = CGPoint(x: point.x - tick_length - astring.size().width, y: point.y - astring.size().height / 2)
+                        drawing_draw_attributed_text(context: context, attributed_text: astring, origin: point_text, horizontal: true)
+                }
+        }
+
+        func draw_axis_1_title(context context: CGContext) {
+                let point = value_to_point(value_1: 0.5, value_2: 0)
+                let astring = astring_font_size_color(string: "False positive rate", font: nil, font_size: 20, color: nil)
+                let point_text = CGPoint(x: point.x - astring.size().width / 2, y: point.y + tick_length + 1.8 * astring.size().height)
+                drawing_draw_attributed_text(context: context, attributed_text: astring, origin: point_text, horizontal: true)
+        }
+
+        func draw_axis_2_title(context context: CGContext) {
+                let point = value_to_point(value_1: 0, value_2: 0.5)
+                let astring = astring_font_size_color(string: "True positive rate", font: nil, font_size: 20, color: nil)
+                let point_text = CGPoint(x: point.x - tick_length - 1.8 * astring.size().height, y: point.y - astring.size().width / 2)
+                drawing_draw_attributed_text(context: context, attributed_text: astring, origin: point_text, horizontal: false)
+        }
+
+
+
 
         func value_to_point(value_1 value_1: Double, value_2: Double) -> CGPoint {
                 let x = CGFloat(value_1) * (box_upper_right.x - box_lower_left.x) + box_lower_left.x
