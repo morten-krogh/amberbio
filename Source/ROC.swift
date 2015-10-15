9e52a6af1f040fba7ab2007d04a4d1294bc5f419
Morten Krogh <morten.krogh@amberbio.com>
2015-10-15 08:53:31 +0200

    ROC

---
+++ Source/ROC.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ROC: TiledScrollViewDelegate {
+
+        var content_size = CGSize.zero
+        var maximum_zoom_scale = 1 as CGFloat
+        var minimum_zoom_scale = 1 as CGFloat
+
+        init() {
+
+
+        }
+
+
+
+        func draw(context context: CGContext, rect: CGRect) {
+
+
+        }
+
+        func scroll_view_did_end_zooming(zoom_scale zoom_scale: CGFloat) {}
+
+
+        func tap_action(location location: CGPoint) {}
+
+
+
+
+
+}
