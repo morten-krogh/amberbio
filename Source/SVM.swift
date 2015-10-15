dea6ea10e7969d3dc47a1e7458e4821ceb574962
Morten Krogh <morten.krogh@amberbio.com>
2015-10-15 18:04:26 +0200

    roc

--- Source/SVM.swift
+++ Source/SVM.swift
@@ -125,6 +125,9 @@ class SVM: SupervisedClassification {
                 let minimum_size = core_sample_indices.count / k_fold
                 let remainder = core_sample_indices.count % k_fold
                 var counter = 0
+
+                var first_training_level_id = 0
+
                 for i in 0 ..< k_fold {
                         let size = minimum_size + (i < remainder ? 1 : 0)
                         var training_sample_indices = [] as [Int]
@@ -150,8 +153,15 @@ class SVM: SupervisedClassification {
                         svm_adapter_train_test(state.values, molecule_indices, molecule_indices.count, state.number_of_samples, training_sample_indices, training_level_ids, training_sample_indices.count, classification_sample_indices, classification_sample_indices.count, &classification_level_ids, &classification_decision_values, kernel.rawValue, linear_C, rbf_C, rbf_gamma)
 
                         test_sample_classified_level_ids += classification_level_ids
-                        test_sample_decision_values += classification_decision_values
-                        
+
+                        if i == 0 {
+                                first_training_level_id = training_level_ids[0]
+                        } else if comparison_level_ids.count == 2 && first_training_level_id != training_level_ids[0] {
+                                test_sample_decision_values += classification_decision_values.map { -$0 }
+                        } else {
+                                test_sample_decision_values += classification_decision_values
+                        }
+
                         counter += size
                 }
         }
