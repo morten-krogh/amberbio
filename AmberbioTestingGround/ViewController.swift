e79a8c3a4d3bf674272edea7d9f8852875239ccd
Morten Krogh <morten.krogh@amberbio.com>
2015-10-16 11:27:18 +0200

    DrawView

--- AmberbioTestingGround/ViewController.swift
+++ AmberbioTestingGround/ViewController.swift
@@ -10,28 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-        let tiled_scroll_view = TiledScrollView()
-
-        var roc: ROC?
+        let draw_view_example = DrawViewExample(frame: CGRect.zero)
 
         override func viewDidLoad() {
                 super.viewDidLoad()
 
-                let decision_values_1 = [1.2, 3, 4]
-                let decision_values_2 = [2, 3.5, 5, 6, 7, 3.6]
-
-                roc = ROC(label_name_1: "label 1", label_name_2: "label 2", decision_values_1: decision_values_1, decision_values_2: decision_values_2)
-                tiled_scroll_view.delegate = roc
-
-                view.addSubview(tiled_scroll_view)
+                view.addSubview(draw_view_example)
         }
 
         override func viewWillLayoutSubviews() {
                 super.viewWillLayoutSubviews()
 
-                tiled_scroll_view.frame = view.bounds
+                draw_view_example.frame = CGRectInset(view.bounds, 70, 70)
+        }
+}
+
+class DrawViewExample: DrawView {
+
+        override init(frame: CGRect) {
+                super.init(frame: frame)
+
+                content_size = CGSize(width: 800, height: 500)
+//                scroll_view.contentSize = content_size
+
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override func draw(context context: CGContext, rect: CGRect) {
+
+                let start_point = CGPoint(x: 0, y: 0)
+                let end_point = CGPoint(x: 800, y: 500)
+                drawing_draw_line(context: context, start_point: start_point, end_point: end_point)
+
+                drawing_draw_circle(context: context, center_x: 150, center_y: 200, radius: 30, color: UIColor.redColor())
+                drawing_draw_circle(context: context, center_x: 750, center_y: 400, radius: 40, color: UIColor.greenColor())
+
         }
 
 
 }
-
--- Source/DrawView.swift
+++ Source/DrawView.swift
@@ -2,22 +2,27 @@ import UIKit
 
 class DrawView: UIView, UIScrollViewDelegate, DrawViewTiledLayerViewDelegate {
 
-        var content_size = CGSize.zero
+        var content_size = CGSize.zero {
+                didSet {
+                        scroll_view.contentSize = content_size
+                }
+        }
+
         var maximum_zoom_scale = 1 as CGFloat
         var minimum_zoom_scale = 1 as CGFloat
         var zoom_scale = 1 as CGFloat
 
         let scroll_view = UIScrollView()
-        var draw_view_tiled_layer_view: DrawViewTiledLayerView!
+        var draw_view_tiled_layer_view: DrawViewTiledLayerView?
 
         override init(frame: CGRect) {
                 super.init(frame: frame)
 
+                draw_view_tiled_layer_view = DrawViewTiledLayerView(frame: CGRect.zero, delegate: self)
+                scroll_view.addSubview(draw_view_tiled_layer_view!)
+
                 scroll_view.delegate = self
                 addSubview(scroll_view)
-
-                draw_view_tiled_layer_view = DrawViewTiledLayerView(frame: CGRect.zero, delegate: self)
-                scroll_view.addSubview(draw_view_tiled_layer_view)
         }
 
         required init?(coder aDecoder: NSCoder) {
@@ -27,6 +32,12 @@ class DrawView: UIView, UIScrollViewDelegate, DrawViewTiledLayerViewDelegate {
         override func layoutSubviews() {
                 super.layoutSubviews()
 
+                set_min_max_zoom_scales()
+                layout_scroll_view()
+
+        }
+
+        func layout_scroll_view() {
                 let (width, height) = (bounds.width, bounds.height)
 
                 let scaled_content_size = CGSizeApplyAffineTransform(content_size, CGAffineTransformMakeScale(zoom_scale, zoom_scale))
@@ -35,33 +46,70 @@ class DrawView: UIView, UIScrollViewDelegate, DrawViewTiledLayerViewDelegate {
                         scroll_view.frame.size.width = scaled_content_size.width
                 } else {
                         scroll_view.frame.origin.x = 0
-                                scroll_view.frame.size.width = bounds.width
-                        }
-                        if scaledContentSize.height < bounds.height {
-                                scroll_view.frame.origin.y = (bounds.height - scaledContentSize.height) / 2.0
-                                scroll_view.frame.size.height = scaledContentSize.height
-                        } else {
-                                scroll_view.frame.origin.y = 0
-                                scroll_view.frame.size.height = bounds.height
-                        }
-                        tiled_view.frame = CGRect(origin: CGPoint.zero, size: scaledContentSize)
+                        scroll_view.frame.size.width = width
                 }
 
+                if scaled_content_size.height < height {
+                        scroll_view.frame.origin.y = (height - scaled_content_size.height) / 2.0
+                        scroll_view.frame.size.height = scaled_content_size.height
+                } else {
+                        scroll_view.frame.origin.y = 0
+                        scroll_view.frame.size.height = height
+                }
 
+                draw_view_tiled_layer_view!.frame = CGRect(origin: CGPoint.zero, size: scaled_content_size)
 
-
-                draw_view_tiled_layer_view.set_levels_of_detail(maximum_zoom_scale: maximum_zoom_scale, minimum_zoom_scale: minimum_zoom_scale)
-
-                
+                draw_view_tiled_layer_view!.set_levels_of_detail(minimum_zoom_scale: minimum_zoom_scale, maximum_zoom_scale: maximum_zoom_scale)
         }
-        
 
+        func set_min_max_zoom_scales() {
+                let (width, height) = (bounds.width, bounds.height)
 
+                let scale_x = width / content_size.width
+                let scale_y = height / content_size.height
 
+                let scale_min = min(1, scale_x, scale_y)
+                if scale_min != minimum_zoom_scale {
+                        minimum_zoom_scale = scale_min
+                        scroll_view.minimumZoomScale = minimum_zoom_scale
+                }
 
+                let scale_max = max(1, scale_x, scale_y)
+                if scale_max != maximum_zoom_scale {
+                        maximum_zoom_scale = scale_max
+                        scroll_view.maximumZoomScale = maximum_zoom_scale
+                }
 
+                if zoom_scale < minimum_zoom_scale {
+                        set_zoom_scale(zoom_scale: minimum_zoom_scale)
+                }
 
+                if zoom_scale > maximum_zoom_scale {
+                        set_zoom_scale(zoom_scale: maximum_zoom_scale)
+                }
 
+                print("scale_min = \(scale_min), scale_max = \(scale_max), zoom_scale = \(zoom_scale)")
+        }
+
+        func set_zoom_scale(zoom_scale zoom_scale: CGFloat) {
+                self.zoom_scale = min(maximum_zoom_scale, max(minimum_zoom_scale, zoom_scale))
+                scroll_view.zoomScale = self.zoom_scale
+        }
+
+        func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+                return draw_view_tiled_layer_view
+        }
+
+        func scrollViewDidZoom(scrollView: UIScrollView) {
+                zoom_scale = scrollView.zoomScale
+                layout_scroll_view()
+        }
+
+        func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+                zoom_scale = scrollView.zoomScale
+                layout_scroll_view()
+                print(zoom_scale)
+        }
 
 
         func draw(context context: CGContext, rect: CGRect) {
@@ -87,7 +135,7 @@ class DrawViewTiledLayerView: UIView {
             fatalError("init(coder:) has not been implemented")
         }
 
-        func set_levels_of_detail(maximum_zoom_scale maximum_zoom_scale: CGFloat, minimum_zoom_scale: CGFloat) {
+        func set_levels_of_detail(minimum_zoom_scale minimum_zoom_scale: CGFloat, maximum_zoom_scale: CGFloat) {
                 (layer as! CATiledLayer).levelsOfDetail = Int(floor(log(maximum_zoom_scale / minimum_zoom_scale) / log(2.0)) + 1)
                 (layer as! CATiledLayer).levelsOfDetailBias = Int(ceil(log(maximum_zoom_scale) / log(2.0)))
                 setNeedsDisplay()
