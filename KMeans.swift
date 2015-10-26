76aa26e9881447cbcc8b6f09c87e5ad8e15a4374
Morten Krogh <morten.krogh@amberbio.com>
2015-10-26 12:26:18 +0100

    k means clustering

--- KMeans.swift
+++ KMeans.swift
@@ -10,6 +10,7 @@ class KMeans {
         var molecule_indices = [] as [Int]
         var cluster_for_sample = [] as [Int]
 
+        var can_cluster = true
         var should_cluster = true
 
         init() {
@@ -18,6 +19,10 @@ class KMeans {
                 var molecule_indices_length = state.number_of_molecules
 
                 knn_molecules_without_missing_values(state.values, state.number_of_molecules, state.number_of_samples, sample_indices, sample_indices.count, &molecule_indices, &molecule_indices_length)
+
+                can_cluster = molecule_indices_length != 0
+                molecule_indices = [Int](molecule_indices[0 ..< molecule_indices_length])
+
                 cluster_for_sample = [Int](count: state.number_of_samples, repeatedValue: 0)
         }
 
@@ -33,7 +38,7 @@ class KMeans {
         }
 
         func cluster() {
-                if should_cluster {
+                if can_cluster && should_cluster {
                         var distance_square = 0.0
                         k_means_clustering(state.values, molecule_indices, molecule_indices.count, state.number_of_samples, k, max_iterations, &cluster_for_sample, &distance_square)
                 }
