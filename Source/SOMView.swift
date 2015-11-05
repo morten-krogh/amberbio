d7cf01ff948d171360230151db7ebcd78c2e7d26
Morten Krogh <morten.krogh@amberbio.com>
2015-11-05 17:58:35 +0100

    self organizing map

--- Source/SOMView.swift
+++ Source/SOMView.swift
@@ -16,7 +16,7 @@ class SOMView: DrawView {
 
         var number_of_rows = 0
         var number_of_columns = 0
-        var som_nodes = [] as [[SOMNode]]
+        var som_nodes = [] as [SOMNode]
 
         init() {
                 super.init(frame: CGRect.zero, tappable: false)
@@ -26,9 +26,9 @@ class SOMView: DrawView {
                 fatalError("init(coder:) has not been implemented")
         }
 
-        func update(som_nodes: [[SOMNode]]) {
-                number_of_rows = som_nodes.count
-                number_of_columns = som_nodes[0].count
+        func update(som_nodes: [SOMNode], number_of_rows: Int, number_of_columns: Int) {
+                self.number_of_rows = number_of_rows
+                self.number_of_columns = number_of_columns
                 self.som_nodes = som_nodes
 
                 let width = margin + (CGFloat(number_of_columns) + (number_of_rows > 1 ? 0.5 : 0)) * sqrt_3 * size_of_hexagon_side + margin
@@ -39,22 +39,20 @@ class SOMView: DrawView {
         }
 
         override func draw(context context: CGContext, rect: CGRect) {
-                for i in 0 ..< number_of_rows {
-                        let origin_y = margin + 1.5 * CGFloat(i) * size_of_hexagon_side
-                        for j in 0 ..< number_of_columns {
-                                let origin_x = margin + (CGFloat(j) + (i % 2 == 0 ? 0 : 0.5)) * sqrt_3 * size_of_hexagon_side
-                                draw_som_node(som_node: som_nodes[i][j], origin_x: origin_x, origin_y: origin_y)
-                        }
+                for som_node in som_nodes {
+                        draw_som_node(som_node: som_node)
                 }
         }
 
-        func draw_som_node(som_node som_node: SOMNode, origin_x: CGFloat, origin_y: CGFloat) {
+        func draw_som_node(som_node som_node: SOMNode) {
+                let origin_y = margin + 1.5 * CGFloat(som_node.row) * size_of_hexagon_side
+                let origin_x = margin + (CGFloat(som_node.column) + (som_node.row % 2 == 0 ? 0 : 0.5)) * sqrt_3 * size_of_hexagon_side
 
-
+                
 
 
         }
-        
+
 
 
 
