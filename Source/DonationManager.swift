0ce12bf0c040cbd77d3d81c535e99ef72bd0f350
Morten Krogh <morten.krogh@amberbio.com>
2015-12-12 20:54:24 +0100

    donation manager

--- Source/DonationManager.swift
+++ Source/DonationManager.swift
@@ -2,6 +2,28 @@ import Foundation
 
 class DonationManager {
 
+        let database: Database
+        
+        var number_of_times_app_did_become_active = 0
+        var number_of_times_app_did_beome_active_at_view = 0
+        var most_recent_time_app_did_become_active = NSDate()
+        var most_recent_time_show_donation_view = NSDate()
+        var most_recent_time_donation: NSDate?
+        
+        init() {
+                database = state.database
+                
+                if let value = get(key: "number_of_times_app_did_become_active") {
+                        number_of_times_app_did_become_active = Int(value) ?? 0
+                } else {
+                        put(key: "number_of_times_app_did_become_active", value: 0)
+                }
+                
+                
+                
+                
+                
+        }
         
         
         
@@ -12,4 +34,12 @@ class DonationManager {
         
         
         
+        
+        func get(key key: String) -> String? {
+                return sqlite_key_value_select(database: database, key: key)
+        }
+        
+        func put(key key: String, value: String) {
+                sqlite_key_value_insert(database: database, key: key, value: value)
+        }
 }
